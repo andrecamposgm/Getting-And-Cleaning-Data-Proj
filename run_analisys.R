b1ded37f9865250f0bc44d2b4474b991fe8851b0
Andre Campos <andrecampos@greenmile.com>
2016-04-10 21:54:58 -0300

    Final adjustments. Generation of tidy data and adding sample data to the ignored folder

--- run_analisys.R
+++ run_analisys.R
@@ -6,12 +6,14 @@
 
 # Check dependencies
 if (!require("data.table")) install.packages("data.table")
-if (!require("dplyr")) install.packages("dplyr")
 
 # A few useful constants
 ZIP_FILE = "projectData.zip"
 DATASET_DIR = "UCI HAR Dataset"
 DOWNLOAD_URL = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+columns <- NULL
+activities_labels <- NULL
 
 #'
 #' Execute basic operations like downloading the experiment data set 
@@ -27,16 +29,18 @@ prepareEnvironment <- function() {
     message(paste("Downloading the file and unziping in:", DATASET_DIR))
     # unfortunaly the dataset dir does not exist. Let's download and unzip the file
     download.file(url = DOWNLOAD_URL, destfile = ZIP_FILE)
-    unzip(ZIP_FILE, overwrite = T, exdir = DATASET_DIR) 
+    unzip(ZIP_FILE, overwrite = T) 
   } 
   # set the working dir to the dataset
   setwd(DATASET_DIR)
 }
 
-# prepeare the data to be understandable
-# list of available features 
-columns <- read.table("features.txt", header = F, stringsAsFactors = F)[,2]
-activities_labels <- fread("activity_labels.txt", header = F, col.names = c("Id", "Activity"))
+loadColumnsAndActivities <- function(){
+  # prepeare the data to be understandable
+  # list of available features 
+  columns <<- read.table("features.txt", header = F, stringsAsFactors = F)[,2]
+  activities_labels <<- fread("activity_labels.txt", header = F, col.names = c("Id", "Activity"))
+}
 
 #'
 #' Gets the file path for each kind of data
@@ -64,7 +68,6 @@ loadDataByContext <- function(context) {
   data <- fread(data_file, header = F, col.names = columns)
   # load subject files and bind the column to the data set
   subjects <- fread(subject_file, header = F, col.names = c("Subject"))
-  data <- data[,Subject := subjects$Subject]
   
   # Requirement: 
   # 2) Extracts only the measurements on the mean and standard deviation for each measurement.
@@ -85,6 +88,8 @@ loadDataByContext <- function(context) {
   # finally add the set of classified activities to the dataset
   data <- data[,Activity := activities$Activity]
   
+  # column bind the subjects
+  data <- data[,Subject := subjects$Subject]
     
   return(data)
 }
@@ -107,29 +112,33 @@ getDescriptiveColNames <- function(colNames) {
 
 
 #' 
-#' Main function containing all basic operation done in this project
+#' Main execution block below continaing all basic operation done in this project
 #' 
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive activity names.
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-# 
-run <- function(){
-  # make sure the data set is downloaded unziped and is the current working dir
-  prepareEnvironment()
-  
-  # Load data sets: Objectives 2 and 3: 
-  test_data  <- loadDataByContext("test")
-  train_data <- loadDataByContext("train")
-  
-  # Merges training and test data sets: Objective 1
-  rbindlist(list(test_data, train_data)) 
-  
-  # Rename variables: Objective 4 
-  descriptiveColNames <- getDescriptiveColNames(names(test_data))
-  names(test_data) <- descriptiveColNames
-  names(train_data) <- descriptiveColNames
-  
-}
+#' 1. Merges the training and the test sets to create one data set.
+#' 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+#' 3. Uses descriptive activity names to name the activities in the data set
+#' 4. Appropriately labels the data set with descriptive activity names.
+#' 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#' 
+ 
+# A few basic steps: make sure the data set is downloaded unziped and it is the current working dir
+prepareEnvironment()
+# load column names and activity labels 
+loadColumnsAndActivities()
 
+# Load data sets: Objectives 2 and 3: 
+test_data  <- loadDataByContext("test")
+train_data <- loadDataByContext("train")
+
+# Merges training and test data sets: Objective 1
+mergedData <- rbindlist(list(test_data, train_data)) 
+
+# Rename variables: Objective 4 
+descriptiveColNames <- getDescriptiveColNames(names(mergedData))
+names(mergedData)   <- descriptiveColNames
+
+# Finally. Objective 5 Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidyData <- with(mergedData, aggregate(mergedData[,1:18, with = F], 
+                                 by = list(Activity, Subject), 
+                                 FUN = mean ))
+write.table(tidyData, "../tidy_data.txt")
